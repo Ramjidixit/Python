e2a872400e8f5a65ebd31c996e91ca3891459bbc
Ram Ji Dixit <100121942+Ramjidixit@users.noreply.github.com>
2022-03-06 13:07:48 +0530

    Add files via upload

---
+++ ASSIGNMENT BASIC PYTHON PROGRAMS/. Program to find the Simple Interest and the total amount when the Principal, Rate of Interest.py
@@ -0,0 +1,8 @@
+""". Program to find the Simple Interest and the total amount when the Principal, Rate of Interest
+	and Time are entered by the user."""
+	
+p=int(input("enter the principle amount:"))
+r=int(input("enter the rate of interest:"))
+t=int(input("enter the time period:"))
+s=(p*r*t)/100
+print("Simple interest=",s)
