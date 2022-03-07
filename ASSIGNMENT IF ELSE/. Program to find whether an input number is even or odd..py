41426c281c8cc4f3c6167c6a1d2a237d6b4e9a47
Ram Ji Dixit <100121942+Ramjidixit@users.noreply.github.com>
2022-03-07 22:54:41 +0530

    Add files via upload

---
+++ ASSIGNMENT IF ELSE/. Program to find whether an input number is even or odd..py
@@ -0,0 +1,7 @@
+"""Program to find whether an input number is even or odd."""
+	
+a=int(input("enter the number  :"))
+if(a%2==0):
+    print("The given number is even.")
+else:
+    print("The given number is odd.")
