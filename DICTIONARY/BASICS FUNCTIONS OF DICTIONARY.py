4859f266f456b645562598655e478ad8368cbaba
Ram Ji Dixit <100121942+Ramjidixit@users.noreply.github.com>
2022-04-18 09:44:56 +0530

    Add files via upload

---
+++ DICTIONARY/BASICS FUNCTIONS OF DICTIONARY.py
@@ -0,0 +1,15 @@
+#BASICS FUNCTIONS OF DICTIONARY
+
+b={"raj":"9559851102","ramesh":"9636521232"}
+b["riya"]="7897399137"      #add new key value pair
+print(b)
+b["raj"]=9645758525        #modify value for a key
+print(b)
+del(b["ramesh"])      #delete a key value pair
+print(b)
+#if you use del(b) it will delete the dictionary object and during runtime it shows error.
+print(len(b))    # len return number of key value pairs
+print(max(b))    # max return maximum key value
+print(min(b))    #min returns minimum key value
+print("raju" in b)  #return true if raju is present in b 
+print("raju" not in b)  #return true if raju is not present in b 
---
+++ DICTIONARY/create a dictionary called students containing names and ages.py
@@ -0,0 +1,8 @@
+"""create a dictionary called students containing names and ages .copy the dictionary into stud.
+empty the student dictionary ,as stud continue to hold the data. """
+
+students={"ram":"25","rahim":"20","radha":"35"}
+stud=students  #to copy dictionary 
+students={} #empty the dictionary
+print(stud)  #it print data of dictionary
+print(students) #it print empty dictionary
