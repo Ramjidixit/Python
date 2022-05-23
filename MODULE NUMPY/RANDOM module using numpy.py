d79fb36f7ef17177c1824b7e41786fb6754ea954
Ram Ji Dixit <100121942+Ramjidixit@users.noreply.github.com>
2022-05-23 18:51:40 +0530

    Add files via upload

---
+++ MODULE NUMPY/RANDOM module using numpy.py
@@ -0,0 +1,9 @@
+#RANDOM module using numpy-it is used to revert the random value in between 0.0 to 0.1 
+import numpy as np 
+a=np.random.rand(5)     #print 5 random numbers
+print(a)
+
+#make 2 d array between 0.1 to 1  it pint array of any random number between 0.0 to 1.0
+import numpy as np 
+a=np.random.rand(5,2) #5 denote row while 2 denote column
+print(a)
