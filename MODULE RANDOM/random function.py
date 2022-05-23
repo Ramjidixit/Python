a9ae4c4c3318f1edda92c16347cdd8b80d72f6d1
Ram Ji Dixit <100121942+Ramjidixit@users.noreply.github.com>
2022-05-23 18:48:46 +0530

    Add files via upload

---
+++ MODULE RANDOM/random function.py
@@ -0,0 +1,4 @@
+#random module-random function is used to revert the random value between 0.0 to 0.1
+import random 
+a=random.random() #print any random number 
+print(a)
---
+++ MODULE RANDOM/shuffle in random module.py
@@ -0,0 +1,7 @@
+#rand range() function---randrange(start,end,stepsize)--end not included
+#randint() function--randint(0,9)--9 is included
+#shuffle()--the shuffle method takes a sequence(list,tuple,string) and recognize the items
+import random 
+l=["orange","apple","mango"]
+random.shuffle(l)
+print(l)
