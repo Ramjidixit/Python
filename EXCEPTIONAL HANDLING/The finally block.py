4290cbc244dfa3a9c86d7d0bbb6f106a5da6bad3
Ram Ji Dixit <100121942+Ramjidixit@users.noreply.github.com>
2022-05-27 20:59:31 +0530

    Add files via upload

---
+++ EXCEPTIONAL HANDLING/The finally block.py
@@ -0,0 +1,8 @@
+#The finally block, if specified, will be executed regardless if the try block raises an error or not.
+try: 
+    print(x)
+except:
+    print("hello")
+finally:
+    print('hiiiiiiiiiiiii')
+#finally statement is compulsorily comes in outcome
---
+++ EXCEPTIONAL HANDLING/To throw (or raise) an exception.py
@@ -0,0 +1,6 @@
+#To throw (or raise) an exception, use the raise keyword.
+#Raise an error and stop the program if x is lower than 0:
+x=-1
+if x<0:
+    raise Exception("no numbers below zero")
+
