c456b4cb68882383faad75618f3061f8f0a65976
Ram Ji Dixit <100121942+Ramjidixit@users.noreply.github.com>
2022-05-31 16:25:58 +0530

    Add files via upload

---
+++ LIST QUESTIONS/multiplication.py
@@ -0,0 +1,6 @@
+"""wap to multiply all the items in a list """
+a=[5,2,6,9,8]
+mul=1
+for x in a:
+    mul*=x
+print(mul)
---
+++ LIST QUESTIONS/sum.py
@@ -0,0 +1,6 @@
+"""a python program to sum all the element of a list"""
+a=[112,3,89,65,35,26,54,67,89,85,23]
+sum=0
+for x in a:
+    sum+=x 
+print(sum)
